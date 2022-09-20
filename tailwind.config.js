3e254623067b81ae8e320899c2d43b0c1498ee5d
shahadat2772 <shahadat2772@gmailcom>
2022-09-20 10:34:50 +0600

    initialized project and intalled tailwind and daisy ui

---
+++ tailwind.config.js
@@ -0,0 +1,33 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#FD841F",
+
+          secondary: "#F57328",
+
+          accent: "#400D51",
+
+          neutral: "#28222A",
+
+          "base-100": "#EAF0F6",
+
+          info: "#888",
+
+          success: "#7FB77E",
+
+          warning: "#E14D2A",
+
+          error: "#CD104D",
+        },
+      },
+    ],
+  },
+  plugins: [require("daisyui")],
+};
