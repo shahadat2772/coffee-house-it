697f91d50cf249c3601a5f6b609f4c4446a8ee7c
shahadat2772 <shahadat2772@gmailcom>
2022-09-20 11:17:49 +0600

    navbar added

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,39 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="navbar bg-base-100">
+      <div className="flex-1">
+        <a className="btn btn-ghost normal-case text-xl">Koffeea</a>
+      </div>
+      <div className="flex-none">
+        <ul className="menu-horizontal p-0">
+          <li className="mx-3 font-medium">
+            <a>coffee</a>
+          </li>
+          <li className="mx-3 flex items-center">
+            <div className="indicator">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
+                />
+              </svg>
+              <span className="badge badge-sm indicator-item">8</span>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
